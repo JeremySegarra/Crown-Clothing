e84c56dc16aee066c40911181e3aac4d7fb36223
jeremy segarra <jeremysegarra92@gmail.com>
2022-06-05 20:06:18 -0400

    adding cart context and shopping icon as well as cart dropdown

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,8 +1,6 @@
-import { useState, useContext } from "react";
+import { useState } from "react";
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
-
-import { UserContext } from "../../contexts/user.context";
 
 import {
   signInWithGooglePopup,
@@ -22,19 +20,14 @@ const SignInForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { email, password } = formFields;
 
-  const { setCurrentUser } = useContext(UserContext);
-
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
   };
 
   const signInWithGoogle = async () => {
-    const { user } = await signInWithGooglePopup();
-
-    createUserDocumentFromAuth(user);
+    await signInWithGooglePopup();
   };
 
-  console.log("testing ssh");
   const handleSubmit = async (event) => {
     event.preventDefault();
 
--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,8 +1,9 @@
-import { async } from "@firebase/util";
-import { logDOM } from "@testing-library/react";
 import { createContext, useState, useEffect } from "react";
 
-import { onAuthStateChangedListener } from "../utils/firebase/firebase.utils";
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth,
+} from "../utils/firebase/firebase.utils";
 
 //as the actual value you want to access
 export const UserContext = createContext({
@@ -17,7 +18,10 @@ export const UserProvider = ({ children }) => {
   useEffect(() => {
     const unsubscripeUser = async () => {
       await onAuthStateChangedListener((user) => {
-        console.log("user", user);
+        if (user) {
+          createUserDocumentFromAuth(user);
+        }
+        setCurrentUser(user);
       });
     };
     return unsubscripeUser;
